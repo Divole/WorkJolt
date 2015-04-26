022f72b1be73d78a03170e6f6d07bd11dd7c4eb0
divole <dvlmmm@gmail.com>
2015-04-26 18:31:03 +0200

    News feed UI

--- PMWJ/news_feed.php
+++ PMWJ/news_feed.php
@@ -1,64 +1,92 @@
 
+<?php $user = $_SESSION['username'];?>|
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>WorkJolt</title>
+    <link rel="stylesheet" type="text/css" href="/WorkJolt/styles/stylesheet.css">
+</head>
 
-  <body>
-    <header>
-     
-    </header>
-    <nav>
-			<?php echo $_SESSION['username'];?>|
-          <a href="?">My News</a>|
+<body class="pattern">
 
-          <a href="?">My Profile</a>|
+  <!-- body background shadow, no content here-->
+  <div class="patter_shade position_absolute body_size "></div>
 
-          <a href="?">My Projects</a>|
-
-          <a href="?">My People</a>|
-
-          <a href="index.php?logout">Log out</a>|
-
-    </nav>
-    <div id="content" style="width:800px; margin:auto;">
-
-      <h2>You can write posts here:</h2>
-      <form action='processPost.php' method='post'>
-        <textarea name='bodytext' id='bodytext' placeholder='Shoutout!'
-        style=' resize: none; width: 800px; height:100px; margin-bottom:5px;'></textarea>
-        <div align='right'>
-          <input id='submitbutton' class='posts post-submit' type='submit' value='Post!'
-          style=' position: relative; width: 120px;'/>
-        </div>
-      </form>
+  <!-- header: contains top menu and logout button-->
+  <div id = "header"class=" absolute_position panel_color panel_shadow">
+    <!-- top menu tabs -->
+    <div id="top_menu" class=" absolute_position">
+      <div id = "my_news" class = "top_menu_item">
+        <a class = "item_selected"href=""> My News </a>
+      </div>
+      <div id = "my_project" class = "top_menu_item">
+        <a class = "item_deselected"href=""> My Project</a>
+      </div>
+      <div id = "my_profile" class = "top_menu_item">
+        <a class = "item_deselected"href=""> My Profile</a>
+      </div>
+      <div id = "my_people" class = "top_menu_item">
+        <a class = "item_deselected"href=""> My People</a>
+      </div>
     </div>
-    <div id="content" style="width:800px; margin:auto;">
-      <h2>Posts:</h2>
-	  
-	  </div>
-<?php 
-	require_once("classes/News.php");
 
-	$news=new News();
-	$posts=$news->getPosts($_SESSION['id']);
-		?><script>console.log(<?php echo var_dump($posts);?>);</script><?php
-	foreach($posts as $i => $value){
-		?><script>console.log('bent');</script><?php
-		$POST_ADDED_BY=$news->getUserName(htmlentities( stripslashes($value[1])));
-		$POST_BODY=htmlentities( stripslashes($value[2]));
-		$POST_DATE=date('l d M Y h:i:s',$value[3]);
-		
-?>
+    <!-- logout button on the header -->
+    <div class="absolute_position position_right" >
+      <div class="btn btn_wrapper"><a class="text" href="../index.php?logout">Logout</a></div>
+    </div>
+  </div>
 
-                             <p style='margin-left: 10px; margin-top: 5px; width:100%;font-size:13px;'>
-                               <?php echo $POST_ADDED_BY." posted this at ".$POST_DATE; ?>
-                             </p>
-                             <br/>
-                             <p style='margin-left: 10px;'><?php echo $POST_BODY;?></p>
+  <!-- side pannels -->
+  <div class="position_absolute body_size">
+    <!-- left side pannel -->
+    <div class="left_panel absolute_position pattern panel_shadow">
+      <div class="patter_shade inner_pannel">
+        Left pannel content
+      </div>
+    </div>
+    <!-- right side pannel -->
+    <div class="right_panel absolute_position pattern panel_shadow">
+      <div class="patter_shade inner_pannel">
+        Targeted advertisments will be here
+      </div>
+    </div>
+  </div>
 
-  </body>
+  <!-- news feed wrapper-center pannel: contains posts -->
+  <div  id="news_wrapper"class="form_wrapper panel_color panel_shadow">
+
+  <div id="content" >
+    <div class="text auto_row">You can write posts here:</div>
+    <form class = "auto_row" action='processPost.php' method='post'>
+      <textarea name='bodytext' id='bodytext' placeholder='Shoutout!'></textarea>
+      <div align='right'>
+        <input id='submitbutton' class='posts post-submit btn' type='submit' value='Post!'/>
+      </div>
+    </form>
+  </div>
+
+
+  <div id="content" class="auto_row">
+    <div class="text">Posts:</div>
+  <?php 
+    require_once("classes/News.php");
+    $news=new News();
+    $posts=$news->getPosts($_SESSION['id']);
+
+    echo "<script>console.log('". var_dump($posts).");</script>";
+    foreach($posts as $i => $value){
+      echo "<script>console.log('bent');</script>";
+      $POST_ADDED_BY=$news->getUserName(htmlentities( stripslashes($value[1])));
+      $POST_BODY=htmlentities( stripslashes($value[2]));
+      $POST_DATE=date('l d M Y h:i:s',$value[3]); 
+      ?>
+      <p style='margin-left: 10px; margin-top: 5px; width:100%;font-size:13px;'>
+      <?php echo $POST_ADDED_BY." posted this at ".$POST_DATE; ?>
+      </p>
+      <br/>
+      <p style='margin-left: 10px;'><?php echo $POST_BODY;?></p>
+    <?php } ?>
+  </div>
+</body>
 </html>
-
-<?php
-	}
-
-?>
